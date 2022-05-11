864a897537c309139802ad81c3082548aa8fcb8e
azhe922 <zsda5858sda@gamil.com>
2022-05-11 13:26:24 +0800

    Cross-Origin Resource Sharing setting

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from api.user_api import user_route
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(user_route)
 
 
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
