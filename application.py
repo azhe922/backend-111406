656be2b2a9927d2fafd2ab585676fd26b068c865
azhe922 <zsda5858sda@gamil.com>
2022-05-11 13:26:34 +0800

    MongoDB connection setting

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from mongoengine import connect
 from api.user_api import user_route
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.register_blueprint(user_route)
 
 
 CORS(app)
+connect(host=os.environ.get('MONGO_URI'))
 
 if __name__ == '__main__':
     app.run(debug=True)
