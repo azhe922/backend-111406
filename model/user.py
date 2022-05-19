09e3bbc498314bebbb4db90bed9b17bdd7331e46
azhe922 <10846006@ntub.edu.tw>
2022-05-20 00:14:14 +0800

    adjust record's and user's data type

--- model/user.py
+++ model/user.py
@@ -1,13 +1,16 @@
-from mongoengine import Document, StringField, EmailField, FloatField, DateTimeField
+from mongoengine import Document, StringField, EmailField, FloatField, DateTimeField, EnumField
+
+from enums.user_role import UserRole
+from enums.gender import Gender
 
 class User(Document):
     user_id = StringField(required=True, max_length=20)
     password = StringField(required=True, max_length=100)
     email = EmailField(required=True, max_length=100)
-    height = FloatField(required=True, max_length=10)
-    weight = FloatField(required=True, max_length=10)
-    sex = StringField(required=True, max_length=1)
+    height = FloatField(max_length=10)
+    weight = FloatField(max_length=10)
+    gender = EnumField(Gender, required=True, max_length=1)
     birthday = StringField(required=True, max_length=10)
-    role = StringField(required=True, max_length=1)
+    role = EnumField(UserRole, required=True, max_length=1)
     create_time = DateTimeField()
     update_time = DateTimeField()
