aebe9beadc6447b26c6ca45624a2fa70c31a82ad
azhe922 <10846006@ntub.edu.tw>
2022-05-20 00:26:09 +0800

    add root_path

--- api/record_api.py
+++ api/record_api.py
@@ -2,8 +2,9 @@ from flask import Blueprint, request
 from service.record_service import add_record_service
 
 record_route = Blueprint('record_route', __name__)
+root_path = "/api/record"
 
-@record_route.route("/api/record", methods=['POST'])
+@record_route.route(root_path, methods=['POST'])
 def add():
     data = request.get_json()
     return add_record_service(data)
--- api/user_api.py
+++ api/user_api.py
@@ -2,12 +2,13 @@ from flask import Blueprint, request
 from service.user_service import signup_service, search_service
 
 user_route = Blueprint('user_route', __name__)
+root_path = "/api/user"
 
-@user_route.route("/api/user/signup", methods=['POST'])
+@user_route.route(f"{root_path}/signup", methods=['POST'])
 def signup():
     data = request.get_json()
     return signup_service(data)
 
-@user_route.route("/api/user", methods=['GET'])
+@user_route.route(root_path, methods=['GET'])
 def search():
     return search_service()
