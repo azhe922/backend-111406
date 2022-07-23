af44b41d70ef1f143c775917f02a19d91240d5f1
azhe922 <10846006@ntub.edu.tw>
2022-07-23 14:38:12 +0800

    add comment and logger

--- api/standard_api.py
+++ api/standard_api.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, make_response, request
 from service.record_service import search_service
+import logging
 
 from service.standard_service import get_times_service
 
 standard_route = Blueprint('standard_route', __name__)
 root_path = "/api/standard"
+logger = logging.getLogger(__name__)
 
-
+# 運動結果數據分析
 @standard_route.route(f"{root_path}/analyze", methods=['POST'])
 def analyze_record():
-    # the parameter which is like (user_id, age, part, gender, times)
+    # the parameters like (user_id, age, part, gender, times)
     data = request.get_json()
     message = ""
     status = 200
@@ -36,10 +38,12 @@ def analyze_record():
         }
         if difference > -100:
             result["difference"] = difference
-        message = "查詢成功"
+        message = "分析成功"
     except Exception as e:
-        message = str(e)
         result = {}
         status = 500
+        errMessage = str(e)
+        logger.error(errMessage)
+        message = "分析失敗，請稍後再試"
     response = make_response({"message": message, "data": result}, status)
     return response
--- api/user_api.py
+++ api/user_api.py
@@ -1,38 +1,50 @@
 from flask import Blueprint, request, make_response
 from service.user_service import signup_service, search_service, get_by_id_service, login_service
+import logging
 
 user_route = Blueprint('user_route', __name__)
 root_path = "/api/user"
+logger = logging.getLogger(__name__)
 
+# 使用者註冊
 @user_route.route(f"{root_path}/signup", methods=['POST'])
 def signup():
     data = request.get_json()
     message = ""
     status = 200
+    logger.info(f"{data['user_id']} 使用者註冊: {data}")
     try:
         signup_service(data)
         message = "註冊成功"
     except Exception as e:
-        message = str(e)
+        errMessage = str(e)
         status = 500
+        logger.error(errMessage)
+        message = "註冊失敗，請稍後再試"
     response = make_response({"message": message}, status)
     return response
 
+# 使用者登入
 @user_route.route(f"{root_path}/login", methods=['POST'])
 def login():
     data = request.get_json()
     message = ""
     status = 200
+    logger.info(f"{data['user_id']} 使用者登入")
     try:
         message = "登入成功" if login_service(data) else "登入失敗，帳號或密碼錯誤"
     except Exception as e:
-        message = str(e)
+        errMessage = str(e)
         status = 500
+        logger.error(errMessage)
+        message = "登入失敗，請稍後再試"
     response = make_response({"message": message}, status)
     return response
 
+# 查詢所有使用者
 @user_route.route(root_path, methods=['GET'])
 def search():
+    logger.info('user search')
     result = []
     message = ""
     status = 200
@@ -40,11 +52,14 @@ def search():
         result = search_service()
         message = "查詢成功"
     except Exception as e:
-        message = str(e)
+        errMessage = str(e)
         status = 500
+        logger.error(errMessage)
+        message = "查詢失敗，請稍後再試"
     response = make_response({"message": message, "data": result}, status)
     return response
 
+# 依ID查詢使用者
 @user_route.route(f"{root_path}/<user_id>", methods=['GET'])
 def get_by_id(user_id):
     result = []
@@ -54,7 +69,9 @@ def get_by_id(user_id):
         result = get_by_id_service(user_id)
         message = "查詢成功"
     except Exception as e:
-        message = str(e)
+        errMessage = str(e)
         status = 500
+        logger.error(errMessage)
+        message = "查詢失敗，請稍後再試"
     response = make_response({"message": message, "data": result}, status)
     return response
