8e23df8b73544ff653c40109f6a8e676d3da61bc
azhe <10846006@ntub.edu.tw>
2022-08-18 19:29:58 +0800

    add model comment

--- app/model/record.py
+++ app/model/record.py
@@ -2,7 +2,23 @@ from mongoengine import Document, StringField, IntField, ListField, EnumField
 
 from app.enums.training_part import TrainingPart
 
+
 class Record(Document):
+    """
+    record-測試紀錄
+
+    |  欄位名稱   |    意義    | 資料型態 |
+    |:-----------:|:----------:|:--------:|
+    |     id      |  流水編號  |  string  |
+    |   user_id   | 使用者帳號 |  string  |
+    |   angles    |    角度    |   list   |
+    | create_time |  紀錄時間  |  string  |
+    |    times    |    次數    |   int    |
+    |    fails     |  失敗次數  |   int    |
+    |    part     |  肌力部位  |   int    |
+    |     pr      |    PR值    |  string  |
+    | test_result |  測試結果  |  string  |
+    """
     user_id = StringField(required=True, max_length=20)
     part = EnumField(TrainingPart)
     times = IntField(required=True, max_length=3)
