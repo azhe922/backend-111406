7aa037dd898b63a33adea4e9e2243029d42af848
azhe <10846006@ntub.edu.tw>
2022-10-21 22:44:30 +0800

    add target_times default

--- app/model/target_usertodo.py
+++ app/model/target_usertodo.py
@@ -14,22 +14,26 @@ class UserTodo(EmbeddedDocument):
     |   complete   |   完成與否   | boolean  | False  |
     | actual_times |   實際次數   |  array  |        |
     """
-    target_times = ListField(required=True)
+    target_times = ListField(required=True, default=[
+        {"times": 15, "set": 2, "total": 30, "part": 0},
+        {"times": 8, "set": 1, "total": 8, "part": 1},
+        {"times": 15, "set": 2, "total": 30, "part": 2}
+    ])
     target_date = StringField(required=True)
     complete = BooleanField(default=False)
     actual_times = ListField(default=[{
         'part': 0,
         'hand': 'left',
         'times': 0,
-    },{
+    }, {
         'part': 0,
         'hand': 'right',
         'times': 0,
-    },{
+    }, {
         'part': 1,
         'hand': 'left',
         'times': 0,
-    },{
+    }, {
         'part': 1,
         'hand': 'right',
         'times': 0,
